ed802849b0361de4891fc730acb884009728c331
antoine <antoine.pascal@gmx.fr>
2026-04-30 15:02:04 +0200

    correction de calculs de dimensions de bloc

--- matrixMultiply.c
+++ matrixMultiply.c
@@ -48,19 +48,20 @@ void matmul(double* A1, double* B1, double *result, int size, int N){
 	// }
 	int i;
 	for (i = 0; i < size; i += N) {
-		int Na = i + N <= size ? N : i + N - size;
+		int Na = i + N <= size ? N : size - i;
 		int j;
 		for (j = 0; j < size; j += N) {
-			int Nb = j + N <= size ? N : j + N - size;
+			int Nb = j + N <= size ? N : size - j;
 			int k;
 			for (k = 0; k < size; k+=N) {
+				int Nk = k + N <= size ? N : size - k;
 				// On fait le produit matriciel : A[i,k] * B[k, j] -> (Na * Nb) * (Nb * Na)
 				// Ensuite on fait : C[i, j] = ...
 				for (int a = 0; a < Na; a++) {
 					for (int b = 0; b < Nb; b++) {
 						// On  fait le produit : C[i, j][a, b] = A[i, k][a, c] * B[k, j][c, b]
-						int sum = 0;
-						for (int c = 0; c < Nb; c++) {
+						double sum = 0;
+						for (int c = 0; c < Nk; c++) {
 							sum += A1[(i + a) * size + k + c] * B1[(k + c) * size + j + b];
 						}
 						result[(i + a) * size + j + b] += sum;
